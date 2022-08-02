ef569afce8747738cb8347556efcf1f168444f2f
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-08-02 10:44:19 +0900

    [추가] 구구단 도메인 추가

---
+++ src/toy01_gugudan/main/java/Gugudan.java
@@ -0,0 +1,23 @@
+package toy01_gugudan.main.java;
+
+public class Gugudan {
+    private final int dan;
+
+    public Gugudan(int dan) {
+        this.dan = dan;
+    }
+
+    public int[] calc(){
+        int[] result = new int[9];
+        for(int i = 0; i < result.length; i++){
+            result[i] = dan * (i+1);
+        }
+        return result;
+    }
+
+    public void print(){
+        for(int i = 0; i < 9; i++){
+            System.out.printf("%d * %d = %d\n", dan, i+1, dan * (i+1));
+        }
+    }
+ }
