c488a8dcc9df3768ca27d0dcf5e9c3bd13ed8d93
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-08-03 17:30:15 +0900

    [추가] 종료번호 추가

--- src/main/java/Gugudan.java
+++ src/main/java/Gugudan.java
@@ -1,23 +1,39 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gugudan {
     private final int dan;
-
+    private final int end;
     public Gugudan(int dan) {
+        this(dan, 9);
+    }
+
+    public Gugudan(int dan, int end) {
         this.dan = dan;
+        this.end = end;
     }
 
     public int[] calc(){
-        int[] result = new int[9];
-        for(int i = 0; i < result.length; i++){
-            result[i] = dan * (i+1);
+        List<Integer> result = new ArrayList<>();
+        for(int i = 1; i <= end; i++){
+            result.add(dan * (i));
+        }
+        return toIntArrays(result);
+    }
+
+    public int[] toIntArrays(List<Integer> list){
+        int[] result = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            result[i] = list.get(i);
         }
         return result;
     }
 
     public void print(){
-        for(int i = 0; i < 9; i++){
-            System.out.printf("%d * %d = %d\n", dan, i+1, dan * (i+1));
+        for(int i = 1; i <= end; i++){
+            System.out.printf("%d * %d = %d\n", dan, i, dan * i);
         }
     }
  }
